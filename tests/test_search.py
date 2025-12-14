e222e14818912dc9b975badbae732e3838a67028
PREM KUMAR REDDY KOTHAKAPU <kpremkumarreddy20011@gmail.com>
2025-12-14 12:44:59 +0530

    Update test_search.py

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,8 +1,11 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-def test_search_pages():
-    response = client.get("/api/v1/search/pages?q=goo")
+def test_search_employees(client):
+    response = client.get(
+        "/api/v1/pages/google/employees/search?name=Rahul"
+    )
     assert response.status_code == 200
+
+    data = response.json()
+    assert "page_id" in data
+    assert data["page_id"] == "google"
+    assert "employees" in data
+    assert isinstance(data["employees"], list)
